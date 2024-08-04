ed98037b319c8fcc17ea578c622c7633b5832179
N-Choorapra <167689627+N-Choorapra@users.noreply.github.com>
2024-08-04 18:37:45 +0530

    Update NoogleDriver.py

--- codes/NoogleDriver.py
+++ codes/NoogleDriver.py
@@ -15,7 +15,7 @@ def proxy_browser():
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument('--disable-extensions')
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
